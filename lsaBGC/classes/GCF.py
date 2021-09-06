33b7dbc48c1c7530209eec44b77fc6676ffe73d7
Rauf Salamzade <salamzader@gmail.com>
2021-09-05 19:30:43 -0500

    Update GCF.py

--- lsaBGC/classes/GCF.py
+++ lsaBGC/classes/GCF.py
@@ -2811,7 +2811,7 @@ def popgen_analysis_of_hg(inputs):
 			csB = CodonSeq(seqB)
 
 			dN, dS = cal_dn_ds(csA, csB)
-			if dN != -1 and dS != -1:
+			if dN != -1 and dS != -1 and dS != 0.0:
 				all_dNdS.append(float(dN)/float(dS))
 
 		if len(all_dNdS) >= (0.75*sample_size):
