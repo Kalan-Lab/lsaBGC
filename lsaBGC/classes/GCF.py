ab51403143cd92bffc812deb4fbbd51682869af2
Rauf Salamzade <salamzader@gmail.com>
2021-09-05 21:40:54 -0500

    Update GCF.py

--- lsaBGC/classes/GCF.py
+++ lsaBGC/classes/GCF.py
@@ -2532,6 +2532,10 @@ def popgen_analysis_of_hg(inputs):
 				b = seqB[p]
 				if a != b and a != '-' and b != '-':
 					differences += 1
+		print(float(differences))
+		print(float(divisor))
+		print(float(differences)/divisor)
+
 		pi = float(differences) / divisor
 
 		"""Calculate s, number of segregation sites)."""
