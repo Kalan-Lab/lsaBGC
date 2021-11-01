72f993ab0b17949e13bab0e20f8e0ec967430891
Rauf Salamzade <salamzader@gmail.com>
2021-11-01 17:54:49 -0500

    Update compareBGCtoGenomeCodonUsage.py

--- scripts/compareBGCtoGenomeCodonUsage.py
+++ scripts/compareBGCtoGenomeCodonUsage.py
@@ -36,6 +36,7 @@ def main():
     print(bgc_genbanks)
     try:
         assert(os.path.isfile(prokka_ffn))
+        print(sum([1 for x in bgc_genbanks if os.path.isfile(x)]))
         assert(sum([1 for x in bgc_genbanks if os.path.isfile(x)]) == len(bgc_genbanks))
     except:
         raise RuntimeError('One or more input files do not exist. Exiting now ...')
