a1e68dde3f30d82a622a6f9e52c460867ad8c5c6
Rauf Salamzade <salamzader@gmail.com>
2022-03-16 15:56:50 -0500

    Update ClusterVisualOfSamples.R

--- lsaBGC/Rscripts/ClusterVisualOfSamples.R
+++ lsaBGC/Rscripts/ClusterVisualOfSamples.R
@@ -9,7 +9,7 @@ pdf_file <- args[3]
 pwdist_data <- read.table(pairwise_distance_file, header=T, sep='\t', row.names=1)
 sainfo_data <- read.table(sample_info_file, header=T, sep='\t')
 
-d <- as.dist(xtabs(pw_difference ~ sample1 + sample2, data=pwdist_data))
+d <- as.dist(xtabs(pw_distance ~ sample1 + sample2, data=pwdist_data))
 pdf(pdf_file, height=10, width=10)
 
 print(d)
@@ -19,9 +19,9 @@ mds.cmdscale$names <- rownames(mds.cmdscale)
 
 plot.df <- merge(mds.cmdscale, sainfo_data, by.x="names", by.y="sample_id")
 print(plot.df)
-#ggplot(plot.df, aes(V1, V2, color=sample_depth, label=names)) +
-#  geom_text(aes(label=names), check_overlap = TRUE, size=2.2,
-#            hjust = "center", vjust = "bottom", nudge_x = 0) +
-#  labs(x="", y="", title="") + scale_fill_gradient() + theme_bw()
+ggplot(plot.df, aes(V1, V2, color=sample_depth, label=names)) +
+  geom_text(aes(label=names), check_overlap = TRUE, size=2.2,
+            hjust = "center", vjust = "bottom", nudge_x = 0) +
+  labs(x="", y="", title="") + scale_fill_gradient() + theme_bw()
 
 dev.off()
