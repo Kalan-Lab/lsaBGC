533415f8f50d09d34f285fb8996ffc927b902678
Rauf Salamzade <salamzader@gmail.com>
2022-03-21 16:22:14 -0500

    Update GCF.py

--- lsaBGC/classes/GCF.py
+++ lsaBGC/classes/GCF.py
@@ -747,15 +747,15 @@ class GCF(Pan):
 				for c in sorted(hg_starts.items(), key=itemgetter(1), reverse=reverse_flag):
 					hgs.append(c[0])
 					if reverse_flag == False:
-						if hg_directions[c] == '+':
-							direction_forward_support[hg] += 1
-						elif hg_direction[c] == '-':
-							direction_reverse_support[hg] += 1
+						if hg_directions[c[0]] == '+':
+							direction_forward_support[c[0]] += 1
+						elif hg_direction[c[0]] == '-':
+							direction_reverse_support[c[0]] += 1
 					else:
-						if hg_directions[c] == '+':
-							direction_reverse_support[hg] += 1
-						elif hg_direction[c] == '-':
-							direction_forward_support[hg] += 1
+						if hg_directions[c[0]] == '+':
+							direction_reverse_support[c[0]] += 1
+						elif hg_direction[c[0]] == '-':
+							direction_forward_support[c[0]] += 1
 
 				for j, hg in enumerate(hgs):
 					all_hgs.add(hg)
